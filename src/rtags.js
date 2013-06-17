bad70913252e8e0ced9b7f3aa78b16dc065b4889
Anders Bakken <agbakken@gmail.com>
2013-06-17 14:30:42 -0700

    Checkpoint

--- src/rtags.js
+++ src/rtags.js
@@ -1,63 +1,160 @@
 var esrefactorContext = new esrefactor.Context();
 
-function rtags-parse-file(code, file, cb)
+var indent = 0;
+function indentString()
+{
+    var ret = "";
+    for (var i=0; i<indent; ++i) {
+        ret += "  ";
+    }
+    return ret;
+}
+function dumpObject(obj)
+{
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key) && typeof obj[key] !== 'function') {
+            log(key);
+        }
+    }
+}
+
+function path(object, scope)
+{
+    if (typeof object === 'string')
+        return object;
+    return typeof object;
+}
+
+function indexFile(code, file, cb)
 {
     var parsed = esprima.parse(code, { loc: true, range: true, tolerant: true });
     if (!parsed) {
         throw new Error('Couldn\'t parse file ' + file + ' ' + code.length);
         return false;
     }
-    context.setCode(syntax);
-    if (!context._syntax)
+    esrefactorContext.setCode(parsed);
+    if (!esrefactorContext._syntax)
         throw new Error('Unable to identify anything without a syntax tree');
-    if (!this._scopeManager)
+    if (!esrefactorContext._scopeManager)
         throw new Error('Unable to identify anything without a valid scope manager');
 
-    var scopeManager = this._scopeManager;
-    var lookup = this._lookup;
+    var scopeManager = esrefactorContext._scopeManager;
+    var lookup = esrefactorContext._lookup;
 
     scopeManager.attach();
     var allRefs = {};
-    estraverse.traverse(this._syntax, {
+    var scope = undefined;
+    var objectScope = [];
+    var prev = undefined;
+    // log(esrefactorContext._syntax);
+    var parents = [];
+    estraverse.traverse(esrefactorContext._syntax, {
         enter: function (node) {
+            parents.push(node);
+            // var old = scope;
             scope = scopeManager.acquire(node) || scope;
             if (node.type === esprima.Syntax.Identifier) {
-                cb(node);
+                var name = objectScope.join(".") || "";
+                if (name.length != 0)
+                    name += ".";
+                name += node.name;
+                log("got node", name, node.loc);
+                // var result = lookup(scope, node);
+
+                // // log("got node ", node.name + " " + (typeof result === 'undefined') + ' ' + (result == node));
+                // if (result) { // && result.identifier == result.declaration) {
+                //     // log("yes", node.name);
+                //     // for (var key in result) {
+                //     //     log(key);
+                //     // }
+                //     // Search for all other identical references (same scope).
+                //     var references = [];
+                //     // scopeManager.attach();
+                //     estraverse.traverse(esrefactorContext._syntax, {
+                //         enter: function (innerNode) {
+                //             // log("entered " + innerNode.name);
+                //             // var scope, i, ref, d;
+                //             var innerScope = scopeManager.acquire(innerNode);
+                //             if (innerScope) {
+                //                 for (i=0; i<innerScope.references.length; ++i) {
+                //                     var ref = innerScope.references[i];
+                //                     if (ref.identifier.name === node.name) {
+                //                         var d = lookup(innerScope, ref.identifier);
+                //                         if (d && d.declaration === result.declaration) {
+                //                             references.push(ref.identifier);
+                //                         }
+                //                     }
+                //                 }
+                //             }
+                //         }
+                //     } );
+                //     // for (var key in scope.variableScope) {
+                //     //     log(key, typeof key);
+                //     // }
+                //     // scopeManager.detach();
+                //     // log("got refs ", node.name, references.length, scope.variableScope.length);
+                // } else {
+                //     log("Got no result", node.name, scope.variableScope.length, dumpObject(node));
+                //     // for (var key in scope.variableScope) {
+                //     //     log(key, typeof scope.variableScope[key]);
+                //     // }
+                //     // for (var key in node) {
+                //     //     log(key, typeof node[key]);
+                //     // }
+                // }
+
+                // cb(node);
             }
+            if (node.type == "ObjectExpression") {
+                objectScope.push(prev.name);
+            } else if (node.type == "MemberExpression") {
+                objectScope.push(path(node.object.name, scope));
+            }
+            if (node && node.name) {
+                prev = node;
+                // log("setting prev", prev);
+            }
+            log(node.type, objectScope.join("."));
+
         },
         leave: function (node) {
+            if (node.type == "ObjectExpression" || node.type == "MemberExpression") {
+                log("popping scope", objectScope.join("."), node.type);
+                objectScope.pop();
+            }
+            parents.pop();
             scope = scopeManager.release(node) || scope;
         }
     });
     scopeManager.detach();
 
-        result = lookup(scope, identifier);
+    // var result = lookup(scope, identifier);
 
-        if (result) {
-            // Search for all other identical references (same scope).
-            result.references = [];
-            scopeManager.attach();
-            estraverse.traverse(this._syntax, {
-                enter: function (node) {
-                    var scope, i, ref, d;
-                    scope = scopeManager.acquire(node);
-                    for (i = 0; i < (scope ? scope.references.length : 0); ++i) {
-                        ref = scope.references[i];
-                        if (ref.identifier.name === identifier.name) {
-                            d = lookup(scope, ref.identifier);
-                            if (d && d.declaration === result.declaration) {
-                                result.references.push(ref.identifier);
-                            }
-                        }
-                    }
-                }
-            });
-            scopeManager.detach();
-        }
+    // if (result) {
+    //     // Search for all other identical references (same scope).
+    //     result.references = [];
+    //         scopeManager.attach();
+    //         estraverse.traverse(this._syntax, {
+    //             enter: function (node) {
+    //                 var scope, i, ref, d;
+    //                 scope = scopeManager.acquire(node);
+    //                 for (i = 0; i < (scope ? scope.references.length : 0); ++i) {
+    //                     ref = scope.references[i];
+    //                     if (ref.identifier.name === identifier.name) {
+    //                         d = lookup(scope, ref.identifier);
+    //                         if (d && d.declaration === result.declaration) {
+    //                             result.references.push(ref.identifier);
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         });
+    //         scopeManager.detach();
+    //     }
 
 
-        return result;
-    };
+    //     return result;
+    // };
 }
 
 
@@ -104,4 +201,4 @@ function rtags-parse-file(code, file, cb)
         //         });
         //     }
         // }
-}
+// }
