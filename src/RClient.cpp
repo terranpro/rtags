af5e2be18ac022b249024ccb75d09b79c75e4bc8
Anders Bakken <agbakken@gmail.com>
2013-07-17 12:51:31 -0700

    Kill builds

--- src/RClient.cpp
+++ src/RClient.cpp
@@ -262,13 +262,12 @@ class QueryCommand : public RCCommand
 {
 public:
     QueryCommand(QueryMessage::Type t, const String &q)
-        : type(t), query(q), extraQueryFlags(0), buildIndex(0)
+        : type(t), query(q), extraQueryFlags(0)
     {}
 
     const QueryMessage::Type type;
     const String query;
     unsigned extraQueryFlags;
-    uint8_t buildIndex;
 
     virtual bool exec(RClient *rc, Client *client)
     {
@@ -278,7 +277,6 @@ public:
         msg.setContext(rc->context());
         msg.setFlags(extraQueryFlags | rc->queryFlags());
         msg.setMax(rc->max());
-        msg.setBuildIndex(buildIndex);
         msg.setPathFilters(rc->pathFilters().toList());
         msg.setRangeFilter(rc->minOffset(), rc->maxOffset());
         msg.setProjects(rc->projects());
@@ -922,7 +920,6 @@ bool RClient::parse(int &argc, char **argv)
                 return false;
             }
             QueryCommand *cmd = addQuery(QueryMessage::PreprocessFile, p);
-            cmd->buildIndex = static_cast<uint8_t>(idx);
             break; }
 
         case RemoveFile: {
